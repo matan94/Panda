82f394014e0bfca0fa500dd4e28a5627b523c140
matan94 <89594256+matan94@users.noreply.github.com>
2021-09-08 20:19:23 +0300

    Add files via upload

---
+++ k8s/deployment.tf
@@ -0,0 +1,41 @@
+resource "kubernetes_namespace" "hello-world" {
+  metadata {
+    name = var.hw_name
+  }
+}
+
+resource "kubernetes_deployment" "hello-world" {
+  metadata {
+    name = var.hw_name
+    #namespace = kubernetes_namespace.hello-world.metadata.0.name
+    labels = {
+      App = var.hw_name
+    }
+  }
+
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = var.hw_name
+      }
+    }
+    template {
+        metadata {
+            labels = {
+            App = var.hw_name
+            }
+        }
+        spec {
+            container {
+            image = var.hw_image
+            name  = var.hw_name
+                port {
+            container_port = 80
+                }
+            }
+        }
+    }
+  }
+}
