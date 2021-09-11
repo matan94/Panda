4305b4a8ccf3280324529959c929ce70856345b4
matan94 <matanon94@gmail.com>
2021-09-12 01:45:14 +0300

    Update terraform.sh

--- Scripts/terraform.sh
+++ Scripts/terraform.sh
@@ -3,13 +3,16 @@
 # Create if didnt exist and Select terraform workspace
 terraform workspace select hello-world || terraform workspace new hello-world
 
-# Create VPC
+# Create VPC and EKS cluster
 cd /terraform-project
 terraform init
 terraform plan
 terraform apply -auto-approve
+terraform output config-map
+terraform output kubeconfig > kubeconfig
+aws eks update-kubeconfig --name matanbpanda-eks-cluster1
 
-# Create EKS cluster
+# Create Hello-World Deployment
 cd /terraform-project/k8s
 terraform init
 terraform plan
